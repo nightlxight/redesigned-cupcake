4437542c9c254eb53901b8d8d656555fb5ff0c7e
nightlxight <itsbeadj@gmail.com>
2021-10-02 18:23:22 +0300

    🍨🍫 Added "invite" command.

---
+++ commands/invite.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
+const yaml = require('js-yaml');
+const settings = yaml.load(fs.readFileSync('./settings', 'utf8'));
+var space = settings.space; var exclamation = settings.exclamation;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Sends an invite link to your bot!'),
+    async execute(interaction) {
+        let inviteLinkEmbed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Invite Link')
+            .setDescription(`${settings.invite_link}`)
+            .setFooter(`Command executed by ${interaction.user.tag}`)
+            .setTimestamp();
+        function sendInviteEmbed() {
+            try {
+                return interaction.reply({embeds: [inviteLinkEmbed] });
+            } catch (error) {
+                // * interaction.reply("There was an error while executing this command!");
+                console.warn("There was an error!");
+                console.error(error);
+            };
+        };
+        sendInviteEmbed();
+    },
+};
